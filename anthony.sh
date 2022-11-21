7cdbe54bbe3b5de94740d30af822da22fadc202f
anthonynwanne <anthony.nwabunwanne@live.com>
2022-11-21 08:48:09 +0100

    xx

--- anthony.sh
+++ anthony.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#script for system analysis
+#This is a script to run system analysis
 sudo apt update -y
-sleep 1
 df -h
-sleep 1
+du -h
+lsblk
+sleep 2
+free -m
 free -g
-sleep 1
 lscpu
-sleep 1
-top -n 2
+sleep 2
+top -n 5
+htop
+echo $?
